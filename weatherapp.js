0c725ddce464bc915987478805d3a8e1f82929c7
bsak350 <bsak350@gmail.com>
2021-04-26 23:29:32 -0700

    Added icon element and function

--- weatherapp.js
+++ weatherapp.js
@@ -44,7 +44,11 @@ function showWeather(response) {
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  iconElement.setAttribute("#icon", response.data.weather[0].icon);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchCity(city) {
