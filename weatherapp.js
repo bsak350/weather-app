41ef104d138b5b525176227d5d97ca344618b0c1
bsak350 <bsak350@gmail.com>
2021-04-30 00:50:54 -0700

    Changed styling (font color, font sizes, background color, border, text alignment, margins, icon size, column length))

--- weatherapp.js
+++ weatherapp.js
@@ -136,7 +136,7 @@ function displayForecast(response) {
                     forecastDay.weather[0].icon
                   }@2x.png"
                   alt=""
-                  width="42"
+                  width="64"
                 />
                 <div class="weather-forecast-temperatures">
                   <span class="weather-forecast-temperature-max"> ${Math.round(
