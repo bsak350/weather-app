ef282f35aae6461378f9420c9470ef549d37cb34
bsak350 <bsak350@gmail.com>
2021-07-06 02:20:53 -0700

    Made adjustments to make page fully responsive on mobile

--- src/weatherapp.js
+++ src/weatherapp.js
@@ -119,7 +119,7 @@ function displayForecast(response) {
       forecastHTML =
         forecastHTML +
         `
-              <div class="col-2">
+              <div class="col-md-2">
                 <div class="weather-forecast-date">${formatDay(
                   forecastDay.dt
                 )}</div>
